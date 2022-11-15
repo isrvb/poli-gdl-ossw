744b1cea2ae0ad2f4f764e78d49c736cf320f616
VB <80378634+isrvb@users.noreply.github.com>
2022-11-14 21:45:20 -0600

    Add files via upload

---
+++ config/global.php
@@ -0,0 +1,23 @@
+<?php 
+//ip de la pc servidor base de datos
+define("DB_HOST", "bmpmhwto4j7buwg7ney8-mysql.services.clever-cloud.com"); 
+// localhost Host: bmpmhwto4j7buwg7ney8-mysql.services.clever-cloud.com
+//Port: 3306
+
+// nombre de la base de datos
+define("DB_NAME", "bmpmhwto4j7buwg7ney8"); // sis_school bmpmhwto4j7buwg7ney8
+
+
+//nombre de usuario de base de datos
+define("DB_USERNAME", "ukceoan89svglw6i"); // phpmyadmin ukceoan89svglw6i
+
+//conraseña del usuario de base de datos
+define("DB_PASSWORD", "INYDnbYaGROtQaz3yGyr"); // 01100s INYDnbYaGROtQaz3yGyr
+
+//codificacion de caracteres
+define("DB_ENCODE", "utf8");
+
+//nombre del proyecto
+define("PRO_NOMBRE", "OSSW");
+ 
+ ?> 
